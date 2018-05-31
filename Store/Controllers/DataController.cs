35cbf9ad1dc1f701e3a1104f5019707fa650b0e6
Nikita <Mansu12135@gmail.com>
2018-05-31 14:07:48 +0300

    initial commit

---
+++ Store/Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using Store.Data;
+using Store.Infrastructure.Providers;
+
+namespace Store.Controllers {
+    [Route("api/v1/[Controller]")]
+    public class DataController : Controller {
+        private readonly IStoreProvider Provider;
+
+        public DataController(IStoreProvider provider) {
+            Provider = provider;
+        }
+
+        [HttpGet("gds")]
+        public async Task<IActionResult> GetGds() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Store/Data/StoreContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Data {
+    public class StoreContext : DbContext {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+        public DbSet<Nomenclature> Nomenclature { get; set; }
+    }
+}
